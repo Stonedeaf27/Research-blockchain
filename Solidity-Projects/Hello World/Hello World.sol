0694bc1293534b7e06b61d6fdfd54d6c643858fb
bhaveshpawar7 <bhavesh.pawar7@gmail.com>
2023-01-29 17:10:23 +0530

    Hello World

---
+++ Solidity-Projects/Hello World/Hello World.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+contract HelloWorld {
+    function helloWorld() external pure returns (string memory) {
+        return "Hello, World!";
+    }
+}
+
+//This code is performed under ethereum official repository
+//SPDX License Expressions provide a way for one to construct expressions that more accurately represent the licensing terms typically found in open source software source code.
