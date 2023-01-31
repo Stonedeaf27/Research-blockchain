8b3fee8c3f455572a36a2ffe83a823ba5fcda5d0
bhaveshpawar7 <bhavesh.pawar7@gmail.com>
2023-01-31 20:39:32 +0530

    Project Completed

--- Solidity-Projects/Lottery (First Project)/Lottery.sol
+++ Solidity-Projects/Lottery (First Project)/Lottery.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.5.0 < 0.9.0;
 
-contract Lottery {              //declare 2 variables manager and participants
+contract Lottery {              //declare 2 variables manager and people
     address public manager;
-    address payable[] public participants;
+    address payable[] public people; //multiple-people
 
     constructor()
     {
@@ -13,6 +13,30 @@ contract Lottery {              //declare 2 variables manager and participants
 
     receive()  external payable // can only be used onece, also be used with external
     {
-        participants.push(msg.sender);
+        require(msg.value== 2 ether);
+        people.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint)
+    {
+        require(msg.sender== manager); // amount will be sent and reflected in manager a/c (contract)
+        return address(this).balance; //see balance in contract
+    }
+
+    function random() public view returns(uint)         //generates a value
+    {  // do not use random in professional smart contracts without proper method
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,people.length)));
+    }
+
+    function selectWinner() public 
+    {
+        require((msg.sender== manager));      
+        require((people.length>=3));    //min number of participant
+        uint r=random(); 
+        address payable winner;
+        uint index = r % people.length;     // (the remainder wont exceed people.length(index value))
+        winner=people[index];   //choooses a index value
+        winner.transfer(getBalance());
+        people=new address payable[](0); //resets the whole system (index)
     }
 }
